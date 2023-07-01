cd44e9d519654998306f128eab31e098dcc606dd
Александр <hyperpositron@gmail.com>
2023-07-01 11:26:01 +0200

    first commit

---
+++ src/main/java/ru/java/java_course_3_5/JavaCourse35Application.java
@@ -0,0 +1,13 @@
+package ru.java.java_course_3_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCourse35Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCourse35Application.class, args);
+	}
+
+}
